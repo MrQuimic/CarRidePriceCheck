34c98d8ff30e66cef9d31de611504d2c606a8cbe
CarlosSantos <100442327+MrQuimic@users.noreply.github.com>
2022-11-27 10:43:39 +0000

    Initial commit 271122 1043

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,16 @@
-module pt.isec.gps.team11.carridepricecheck {
+module com.example.demo1810 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
     requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
 
-    opens pt.isec.gps.team11.carridepricecheck to javafx.fxml;
-    exports pt.isec.gps.team11.carridepricecheck;
+    opens pt.isec.gps.team11 to javafx.fxml;
+    exports pt.isec.gps.team11;
 }
---
+++ src/main/java/pt/isec/gps/team11/MainController.java
@@ -0,0 +1,14 @@
+package pt.isec.gps.team11;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
