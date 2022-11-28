558dfdc301df08a7de7ede132a82b889092cda69
Luiisdscruz <85038285+Luiisdscruz@users.noreply.github.com>
2022-11-28 13:58:54 +0000

    View from bookForm done

--- src/main/java/pt/isec/gps/team11/gui/panes/utils/BookForm.java
+++ src/main/java/pt/isec/gps/team11/gui/panes/utils/BookForm.java
@@ -1,19 +1,24 @@
 package pt.isec.gps.team11.gui.panes.utils;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import pt.isec.gps.team11.MyBrowser;
-import pt.isec.gps.team11.gui.MenuOpt;
 import pt.isec.gps.team11.model.CRPCManager;
-import pt.isec.gps.team11.model.fsm.States;
 
 public class BookForm {
     CRPCManager crpcManager;
+    VBox vbAdressesAndOptions, vbAdresses, vbOptions, vbAdressesWithTitle, vbOptionsWithTitle;
+    Label lbAdressesTitle, lbOptionsTitle;
+
+    VBox vbStartAdress, vbEndAdress, vbDirections, vbExtraWaitTime, vbPassengers, vbSuitcases, vbDepartureDate, vbDepartureTime, vbTolls;
+    Label lbStartAdress, lbEndAdress, lbDirections, lbExtraWaitTime, lbPassengers, lbSuitcases, lbDepartureDate, lbDepartureTime, lbTolls;
+
+    HBox hbPassengersSuitcases, hbDepartureDateAndImage, hbDepartureTimeAndImage;
+
+    TextField tfStartAdress, tfEndAdress, tfExtraWaitTime, tfDepartureTime;
+    ChoiceBox cbDirections, cbPassengers, cbSuitcases, cbTolls;
+    DatePicker dpDepartureDate;
 
     public BookForm(CRPCManager crpcManager){
         this.crpcManager = crpcManager;
@@ -24,11 +29,167 @@ public class BookForm {
     }
 
     private void createViews() {
+        //VBox of the Addresses and the Options
+        vbAdressesAndOptions = new VBox();
+        vbAdressesAndOptions.setAlignment(Pos.CENTER_LEFT);
 
+        //Adresses VBox With Title
+        vbAdressesWithTitle = new VBox();
+        vbAdresses.setAlignment(Pos.CENTER);
+
+        lbAdressesTitle = new Label("Adresses");
+
+        //Adresses VBox
+        vbAdresses = new VBox();
+        vbAdresses.setAlignment(Pos.CENTER);
+
+        //Start Adress
+        vbStartAdress = new VBox();
+        vbStartAdress.setAlignment(Pos.CENTER_LEFT);
+
+        lbStartAdress = new Label("Start adress");
+        lbStartAdress.setAlignment(Pos.CENTER_LEFT);
+        tfStartAdress = new TextField();
+        tfStartAdress.setPromptText("Introduce start address");
+
+        vbStartAdress.getChildren().addAll(lbStartAdress,tfStartAdress);
+
+        //End Adress
+        vbEndAdress = new VBox();
+        vbEndAdress.setAlignment(Pos.CENTER_LEFT);
+
+        lbEndAdress = new Label("Start adress");
+        lbEndAdress.setAlignment(Pos.CENTER_LEFT);
+        tfEndAdress = new TextField();
+        tfEndAdress.setPromptText("Introduce end address");
+
+        vbEndAdress.getChildren().addAll(lbEndAdress,tfEndAdress);
+
+
+        vbAdresses.getChildren().addAll(vbStartAdress, vbEndAdress);
+
+        vbAdressesWithTitle.getChildren().addAll(lbAdressesTitle,vbAdresses);
+
+        //Options VBox With Title
+        vbOptionsWithTitle = new VBox();
+        vbOptions.setAlignment(Pos.CENTER);
+
+        lbOptionsTitle = new Label("Options");
+
+        //Options VBox
+        vbOptions = new VBox();
+        vbOptions.setAlignment(Pos.CENTER);
+
+        //Directions
+        vbDirections = new VBox();
+        vbDirections.setAlignment(Pos.CENTER_LEFT);
+
+        lbDirections = new Label("Directions");
+        lbDirections.setAlignment(Pos.CENTER_LEFT);
+        cbDirections = new ChoiceBox();
+        //add items to choiceBox
+
+        vbDirections.getChildren().addAll(lbDirections,cbDirections);
+
+        //Extra Waiting Time
+        vbExtraWaitTime = new VBox();
+        vbExtraWaitTime.setAlignment(Pos.CENTER_LEFT);
+
+        lbExtraWaitTime = new Label("Extra Waiting Time (minutes)");
+        lbExtraWaitTime.setAlignment(Pos.CENTER_LEFT);
+        tfExtraWaitTime = new TextField();
+        tfExtraWaitTime.setPromptText("0 minutes");
+
+        vbExtraWaitTime.getChildren().addAll(lbExtraWaitTime,tfExtraWaitTime);
+
+        //HBox for Passengers and Suitcases
+        hbPassengersSuitcases = new HBox();
+        hbPassengersSuitcases.setAlignment(Pos.CENTER_LEFT);
+
+        //Passengers
+        vbPassengers = new VBox();
+        vbPassengers.setAlignment(Pos.CENTER);
+
+        lbPassengers = new Label("Passengers");
+        lbPassengers.setAlignment(Pos.CENTER_LEFT);
+        cbPassengers = new ChoiceBox();
+        //add items to choiceBox
+
+        vbPassengers.getChildren().addAll(lbPassengers,cbPassengers);
+
+        //Suitcases
+        vbSuitcases = new VBox();
+        vbSuitcases.setAlignment(Pos.CENTER);
+
+        lbSuitcases = new Label("Suitcases");
+        lbSuitcases.setAlignment(Pos.CENTER_LEFT);
+        cbSuitcases = new ChoiceBox();
+        //add items to choiceBox
+
+        vbSuitcases.getChildren().addAll(lbSuitcases,cbSuitcases);
+
+        hbPassengersSuitcases.getChildren().addAll(vbPassengers,vbSuitcases);
+
+        //HBox for Departure Date and Image
+        hbDepartureDateAndImage = new HBox();
+        hbDepartureDateAndImage.setAlignment(Pos.CENTER_LEFT);
+
+        //Departure Date
+        vbDepartureDate = new VBox();
+        vbDepartureDate.setAlignment(Pos.CENTER);
+
+        lbDepartureDate = new Label("Departure date");
+        lbDepartureDate.setAlignment(Pos.CENTER_LEFT);
+        dpDepartureDate = new DatePicker();
+
+
+        vbDepartureDate.getChildren().addAll(lbDepartureDate,dpDepartureDate);
+
+        //parte de colocar a imagem aqui
+
+
+        hbDepartureDateAndImage.getChildren().addAll(vbDepartureDate/*,cena da imagem*/);
+
+        //HBox for Departure Time and Image
+        hbDepartureTimeAndImage = new HBox();
+        hbDepartureTimeAndImage.setAlignment(Pos.CENTER_LEFT);
+
+        //Departure Time
+        vbDepartureTime = new VBox();
+        vbDepartureTime.setAlignment(Pos.CENTER);
+
+        lbDepartureTime = new Label("Departure Time");
+        lbDepartureTime.setAlignment(Pos.CENTER_LEFT);
+        tfDepartureTime = new TextField();
+        tfDepartureTime.setPromptText("Time");
+
+        vbDepartureTime.getChildren().addAll(lbDepartureTime,tfDepartureTime);
+
+        //parte de colocar a imagem aqui
+
+
+        hbDepartureDateAndImage.getChildren().addAll(vbDepartureTime/*,cena da imagem*/);
+
+        //Tolls
+        vbTolls = new VBox();
+        vbTolls.setAlignment(Pos.CENTER);
+
+        lbTolls = new Label("Tolls:");
+        lbTolls.setAlignment(Pos.CENTER_LEFT);
+        cbTolls = new ChoiceBox();
+        //add items to choiceBox
+
+        vbOptions.getChildren().addAll(vbDirections,vbExtraWaitTime,hbPassengersSuitcases,hbDepartureDateAndImage,hbDepartureTimeAndImage,vbTolls);
+
+        vbOptionsWithTitle.getChildren().addAll(lbOptionsTitle,vbOptions);
+
+        vbAdressesAndOptions.getChildren().addAll(vbAdressesWithTitle,vbOptions);
     }
 
     private void registerHandlers() {
-
+        crpcManager.addPropertyChangeListener(evt -> {
+            update();
+        });
     }
 
     private void configAdapter() {
