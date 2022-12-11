12e4cb83beff82bf69ada413bde2a2dd19a4cfa5
leosousa200 <113478737+leosousa200@users.noreply.github.com>
2022-12-11 19:11:54 +0000

    Update BookingListPane.java

--- src/main/java/pt/isec/gps/team11/gui/panes/BookingListPane.java
+++ src/main/java/pt/isec/gps/team11/gui/panes/BookingListPane.java
@@ -1,12 +1,15 @@
 package pt.isec.gps.team11.gui.panes;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import pt.isec.gps.team11.gui.MenuOpt;
 import pt.isec.gps.team11.gui.panes.utils.ImageManager;
 import pt.isec.gps.team11.model.CRPCManager;
@@ -16,7 +19,7 @@ import pt.isec.gps.team11.model.fsm.States;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class BookingListPane extends BorderPane {
+public class BookingListPane extends StackPane {
     CRPCManager crpcManager;
     private ArrayList<Trip> trips;
     private HBox[] tripsInfos;
@@ -46,42 +49,39 @@ public class BookingListPane extends BorderPane {
 
             /*                                      vbox1                                                       */
             Label car = new Label();
-            car.setText("Car: "+ currentTrip.getCar().getBrand() + " " + currentTrip.getCar().getModel());
+            car.setText("Car:\t"+ currentTrip.getCar().getBrand() + " " + currentTrip.getCar().getModel() +'\t');
             Label licensePlate = new Label();
-            licensePlate.setText("License plate: " + currentTrip.getCar().getLicensePlate());
+            licensePlate.setText("License plate:\t" + currentTrip.getCar().getLicensePlate()+'\t');
             Label directions = new Label();
             String label3String = currentTrip.isOneWay() ? "One way" : "Comeback";
-            directions.setText("Directions: " + label3String);
+            directions.setText("Directions:\t" + label3String+'\t');
             vbox1.getChildren().addAll(car, licensePlate, directions);
+
             /*                                      vbox2                                                       */
             Label startAddress = new Label();
-            startAddress.setText("Start address: " + currentTrip.getOrigin());
+            startAddress.setText("Start address:\t" + currentTrip.getOrigin()+'\t');
             Label endAddress = new Label();
-            endAddress.setText("End address: " + currentTrip.getDestination());
+            endAddress.setText("End address:\t" + currentTrip.getDestination()+'\t');
             Label departureTime = new Label();
-            departureTime.setText("Departure Time: " + currentTrip.getStringDate() + " " + currentTrip.getStringTime());
+            departureTime.setText("Departure Time:\t" + currentTrip.getStringDate() + " " + currentTrip.getStringTime()+'\t');
             Label waitingTime = new Label();
-            waitingTime.setText("Waiting time: " + currentTrip.getExtraWaitingTime());
+            waitingTime.setText("Waiting time:\t" + currentTrip.getExtraWaitingTime()+'\t');
             vbox2.getChildren().addAll(startAddress, endAddress, departureTime, waitingTime);
             /*                                      vbox3                                                       */
             Label kilometers = new Label();
-            kilometers.setText("kilometers: " + currentTrip.getDistance());
+            kilometers.setText("kilometers:\t" + currentTrip.getDistance()+'\t');
             Label price = new Label();
-            endAddress.setText("Price: " + currentTrip.getPrice());
+            endAddress.setText("Price:\t" + currentTrip.getPrice()+'\t');
             Label passengers = new Label();
-            passengers.setText("Passengers: " + currentTrip.getNumberOfPassengers() );
+            passengers.setText("Passengers:\t" + currentTrip.getNumberOfPassengers() +'\t');
             Label suitcases = new Label();
-            suitcases.setText("Suitcases: " + currentTrip.getNumberOfLuggage());
+            suitcases.setText("Suitcases:\t" + currentTrip.getNumberOfLuggage()+'\t');
             vbox3.getChildren().addAll(kilometers, price, passengers, suitcases);
-
+            suitcases.setTextAlignment(TextAlignment.CENTER);
             tripsInfos[i].getChildren().addAll(vbox1, vbox2, vbox3);
             vbFirst.getChildren().add(tripsInfos[i]);
-
-
-
         }
-
-        this.setTop(vbFirst);
+        this.getChildren().add(vbFirst);
     }
 
     private void registerHandlers() {
